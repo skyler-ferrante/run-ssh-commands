b09b3001034acf09f1869499e87bc996cd2f6c16
skyler-ferrante <sjf5462@rit.edu>
2021-12-14 19:29:41 -0500

    Executable is now named ssh_run instead of a.out

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # Shouuld probably make a makefile at somepoint
 # But this works for now
 
-gcc src/ssh_run.c src/ssh_run.h src/main.c -lssh
+gcc src/ssh_run.c src/ssh_run.h src/main.c -lssh -o ssh_run
